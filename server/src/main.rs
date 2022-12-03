91da3061fd77b58b2047993bbb20184ec8c06792
Vjg9 <vojin09jov@gmail.com>
2022-12-03 13:01:20 +0100

    Added Logging to code

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,5 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{get, web, App, HttpServer, Responder, middleware::Logger};
+use dotenvy::dotenv;
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -7,14 +8,31 @@ async fn index() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    // Logging
+    dotenv().ok();
+    if std::env::var("MODE").unwrap() == "debug" {
+        std::env::set_var("RUST_LOG", "debug");
+        std::env::set_var("RUST_BACKTRACE", "1");
+    }
+    env_logger::init();
+
+    // Get port
+    let port: u16 = std::env::var("PORT").unwrap().parse::<u16>().unwrap();
+
+    // Show server data
+    println!("[\x1b[32mINFO\x1b[37m] Server running on port: \x1b[34m{port}\x1b[37m; [\x1b[34mhttp://localhost:{port}\x1b[37m]");
+
+    // Create HttpServer
+    HttpServer::new(move || {
+        let logger = Logger::default();
         App::new()
         .service(
             web::scope("/api")
                 .service(index)
         )
+        .wrap(logger)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("127.0.0.1", port))?
     .run()
     .await
 }

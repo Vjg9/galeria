9921045f1ec5a111afcaeffc60f99f42539a0e43
Vjg9 <vojin09jov@gmail.com>
2022-12-04 13:39:47 +0100

    Added init() function to generate tables if they don't exist

---
+++ server/src/db/mod.rs
@@ -0,0 +1,32 @@
+use sqlx::postgres::{PgPoolOptions, PgRow};
+use sqlx::{FromRow, Row};
+
+// Ititialize the database
+pub async fn init() -> Result<(), sqlx::Error> {
+   // Create connection
+   let pool = PgPoolOptions::new()
+       .max_connections(5)
+       .connect("postgres://postgres:postgres@localhost/static")
+       .await?;
+
+   // Create tabels if they don't exist
+   sqlx::query(
+        r#"CREATE TABLE IF NOT EXISTS profile ( id INTEGER PRIMARY KEY, name TEXT);"#
+    )
+       .execute(&pool)
+       .await?;
+
+    sqlx::query(
+        r#"CREATE TABLE IF NOT EXISTS album ( id INTEGER PRIMARY KEY, name TEXT, profile INTEGER NOT NULL, FOREIGN KEY(profile) REFERENCES profile(id) ON DELETE CASCADE);"#
+    )
+       .execute(&pool)
+       .await?;
+
+    sqlx::query(
+        r#"CREATE TABLE IF NOT EXISTS image ( id INTEGER PRIMARY KEY, name TEXT, album INTEGER NOT NULL, FOREIGN KEY(album) REFERENCES album(id) ON DELETE CASCADE);"#
+    )
+       .execute(&pool)
+       .await?;
+
+    Ok(()) 
+}

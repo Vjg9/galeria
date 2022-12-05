bf53dc1d017755c9f8f1843d728f4dba10c19f8b
Vjg9 <vojin09jov@gmail.com>
2022-12-05 16:03:28 +0100

    Changed id from INTEGER to SERIAL and used db url env var

--- server/src/db/mod.rs
+++ server/src/db/mod.rs
@@ -1,4 +1,5 @@
 use sqlx::postgres::PgPoolOptions;
+use dotenvy::dotenv;
 
 // Model modules
 pub mod profile;
@@ -7,27 +8,30 @@ pub mod image;
 
 // Ititialize the database
 pub async fn init() -> Result<sqlx::Pool<sqlx::Postgres>, sqlx::Error> {
+    dotenv().ok();
+    let url = std::env::var("DATABASE_URL").unwrap();
+
    // Create connection
    let pool = PgPoolOptions::new()
        .max_connections(5)
-       .connect("postgres://postgres:postgres@localhost/static")
+       .connect(&url)
        .await?;
 
    // Create tabels if they don't exist
    sqlx::query(
-        r#"CREATE TABLE IF NOT EXISTS profile ( id INTEGER PRIMARY KEY, name TEXT);"#
+        r#"CREATE TABLE IF NOT EXISTS profile ( id SERIAL PRIMARY KEY, name TEXT);"#
     )
        .execute(&pool)
        .await?;
 
     sqlx::query(
-        r#"CREATE TABLE IF NOT EXISTS album ( id INTEGER PRIMARY KEY, name TEXT, profile INTEGER NOT NULL, FOREIGN KEY(profile) REFERENCES profile(id) ON DELETE CASCADE);"#
+        r#"CREATE TABLE IF NOT EXISTS album ( id SERIAL PRIMARY KEY, name TEXT, profile INTEGER NOT NULL, FOREIGN KEY(profile) REFERENCES profile(id) ON DELETE CASCADE);"#
     )
        .execute(&pool)
        .await?;
 
     sqlx::query(
-        r#"CREATE TABLE IF NOT EXISTS image ( id INTEGER PRIMARY KEY, name TEXT, album INTEGER NOT NULL, FOREIGN KEY(album) REFERENCES album(id) ON DELETE CASCADE);"#
+        r#"CREATE TABLE IF NOT EXISTS image ( id SERIAL PRIMARY KEY, name TEXT, album INTEGER NOT NULL, FOREIGN KEY(album) REFERENCES album(id) ON DELETE CASCADE);"#
     )
        .execute(&pool)
        .await?;

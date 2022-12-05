6ca343a50fc77a843a8ea274b066fb815720eadb
Vjg9 <vojin09jov@gmail.com>
2022-12-05 20:11:01 +0100

    Added list albums query to profile

--- server/src/db/profile.rs
+++ server/src/db/profile.rs
@@ -2,7 +2,7 @@ use sqlx::{
     Postgres,
     Pool
 };
-use crate::models::Profile;
+use crate::models::{Profile, Album};
 
 // List profiles
 pub async fn list(pool: &Pool<Postgres>) -> Result<Vec<Profile>, sqlx::Error> {
@@ -53,4 +53,23 @@ pub async fn update(pool: &Pool<Postgres>, id: i32, name: String) {
         .execute(pool)
         .await
         .unwrap();
+}
+
+// List profile's albums
+pub async fn list_albums(pool: &Pool<Postgres>, id: i32) -> Result<Vec<Album>, sqlx::Error>{
+    let rows: Vec<(i32, String, i32)> = sqlx::query_as(r#"SELECT * FROM album WHERE profile=$1"#)
+        .bind(id)
+        .fetch_all(pool)
+        .await?;
+
+    let albums: Vec<Album> = rows.iter().map(|row| {
+        Album {
+            id: row.0,
+            name: row.1.as_str().to_string(),
+            profile: row.2
+        }
+    })
+    .collect();
+
+    Ok(albums)
 }
